fcee046c39f0c6a2752d54fab313a73d2e5dfee2
tarabarico <tarabarico@gmail.com>
2022-07-10 21:58:28 -0400

    Added ability to save hero. Modified how data is retrieved from backend

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,23 +3,43 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+  httpOptions = { headers :new HttpHeaders({'Content=Type': 'application/json'})};
 
-  constructor(private messageService :MessageService) { }
+  constructor(private messageService :MessageService, private http :HttpClient) { }
 
   getHeroes() :Observable<Hero[]> {
-    const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap(_ => this.log('fetched heroes')), catchError(this.handleError<Hero[]>('getHeroes', [])));
   }
 
   getHero(id :number) :Observable<Hero> {
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`)));
+  }
+
+  private log(message :string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result? :T) {
+    return (error :any) :Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
+  }
+
+  updateHero(hero :Hero) :Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(tap(_ => this.log(`Updated hero id=${hero.id}`)), catchError(this.handleError<any>('updateHero')));
   }
 }
